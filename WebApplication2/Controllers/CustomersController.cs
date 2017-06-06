7d83e52817cf21720714eb3059ff8dbffc50dd6f
elen elen <elen elen>
2017-06-06 18:12:19 +0300

    initial commit to add .gitignore

---
+++ WebApplication2/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer {Name="John Smiths"},
+                new Customer {Name="Mary Williams"}
+            };
+            ViewBag.customers = customers;
+            return View();
+        }
+    }
+}
